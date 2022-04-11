d2df07e9239965c068ef5623aca7f1e8b4064c10
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-11 22:25:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="auth-form-container ">
+    <div className="auth-form">
+      <h1>Sign Up</h1>
+      <form >
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <div className="input-wrapper">
+            <input
+              
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <div className="input-wrapper">
+            <input
+              
+              type="password"
+              name="password"
+              id="password"
+            />
+          </div>
+          
+        </div>
+        <div className="input-field">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <div className="input-wrapper">
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirm-password"
+              
+            />
+          </div>
+         
+        </div>
+        <button type="submit" className="auth-form-submit">
+          Sign Up
+        </button>
+      </form>
+      <p className="redirect">
+        Already have an account?{" "}
+        <span>Login</span>
+      </p>
+      <div className="horizontal-divider">
+        <div className="line-left" />
+        <p>or</p>
+        <div className="line-right" />
+      </div>
+      <div className="input-wrapper">
+        <button  className="google-auth">
+          
+          <p> Continue with Google </p>
+        </button>
+      </div>
     </div>
+  </div>
   );
 }
 
